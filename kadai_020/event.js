1aba6eb4afb8dcfa9b9eeb8abb478ca2e8da4803
MegumiMatsudaira <160100899+MegumiMatsudaira@users.noreply.github.com>
2024-04-08 12:43:31 +0900

    課題　DOM操作を使用して文字表示を変えよう

---
+++ kadai_020/event.js
@@ -0,0 +1,7 @@
+const chengeBtn = document.getElementById('btn');
+const text = document.getElementById('text');
+
+chengeBtn.addEventListener('click' , ()=> {
+  text.textContent = 'ボタンをクリックしました';
+
+});
